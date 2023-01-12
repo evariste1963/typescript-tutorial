c330a84d6a422cddfabee39886d2f50290e3c83a
evariste1963 <evariste@hotmail.co.uk>
2023-01-12 20:30:19 +0000

    up to lesson 4

--- sandbox.js
+++ sandbox.js
@@ -1,1 +1,9 @@
 var character = 'mario';
+var age = 30;
+var isBlackBelt = false;
+//safe typing functions
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(Math.PI * 3));
+console.log(circ(7.5));
--- sandbox.ts
+++ sandbox.ts
@@ -1,4 +1,12 @@
 let character = 'mario';
-let age = 30
-let isBlackBelt = false
+let age = 30;
+let isBlackBelt = false;
 
+//safe typing functions
+
+const circ = (diameter: number) =>{
+  return diameter * Math.PI
+}
+
+console.log(circ(Math.PI * 3))
+console.log(circ(7.5))
